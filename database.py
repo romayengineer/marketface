c55ece89d6b4ae7b4ee1763ac47c84a8173715b6
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-08-31 08:42:57 -0300

    * if the database is not running print error message saying run pocketbase serve and exit with status code 1

--- database.py
+++ database.py
@@ -1,11 +1,20 @@
+import sys
 from pocketbase import PocketBase
+from pocketbase.utils import ClientResponseError
 
 client = PocketBase('http://127.0.0.1:8090')
 
-# this is not a real password
-admin_data = client.admins.auth_with_password("romayengineer@gmail.com", "adminadmin!")
+admin_data = None
 
-assert admin_data.is_valid, "invalid database credentials"
+try:
+    # this is not a real password
+    admin_data = client.admins.auth_with_password("romayengineer@gmail.com", "adminadmin!")
+except ClientResponseError:
+    print("did you run pocketbase serve? from data/base")
+    sys.exit(1)
+
+
+assert admin_data and admin_data.is_valid, "invalid database credentials"
 
 def get_first_item(query_filter):
     return client.collection("items").get_first_list_item(query_filter)
