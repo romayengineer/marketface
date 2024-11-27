2b43380abd96bb34b7868251877d03a87db4f1f1
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-11-27 16:24:39 -0300

    remove Pipfile.lock

--- marketface/database.py
+++ marketface/database.py
@@ -1,16 +1,19 @@
 import sys
 from typing import Dict
+
 from pocketbase import PocketBase
 from pocketbase.utils import ClientResponseError
 
-#TODO use a config file for this
-client = PocketBase('http://127.0.0.1:8090')
+# TODO use a config file for this
+client = PocketBase("http://127.0.0.1:8090")
 
 admin_data = None
 
 try:
     # this is not a real password
-    admin_data = client.admins.auth_with_password("romayengineer@gmail.com", "adminadmin!")
+    admin_data = client.admins.auth_with_password(
+        "romayengineer@gmail.com", "adminadmin!"
+    )
 except ClientResponseError:
     print("did you run pocketbase serve? from data/base")
     sys.exit(1)
@@ -18,11 +21,14 @@ except ClientResponseError:
 
 assert admin_data and admin_data.is_valid, "invalid database credentials"
 
+
 def get_first_item(query_filter):
     return client.collection("items").get_first_list_item(query_filter)
 
+
 def get_item_by_url(url):
     return get_first_item(f'url = "{url}"')
+
 
 def get_items_list(start, count, filter=None):
     params = {}
@@ -30,12 +36,14 @@ def get_items_list(start, count, filter=None):
         params["filter"] = filter
     return client.collection("items").get_list(start, count, params)
 
+
 def get_items_incomplete(start, count):
     """
     returns the list of items missing
     title description price and other details
     """
     return get_items_list(start, count, 'title = ""')
+
 
 def update_item_by_url(url: str, body_params: Dict) -> bool:
     record = get_item_by_url(url)
@@ -45,15 +53,17 @@ def update_item_by_url(url: str, body_params: Dict) -> bool:
     client.collection("items").update(rid, body_params)
     return True
 
+
 def update_item_deleted(url: str) -> bool:
-    body_params = {
-        "deleted": True
-    }
+    body_params = {"deleted": True}
     print("marked as deleted! ", url)
     return update_item_by_url(url, body_params)
 
+
 def create_item(url, img_path):
-    client.collection("items").create({
-        "url": url,
-        "img_path": img_path,
-    })
+    client.collection("items").create(
+        {
+            "url": url,
+            "img_path": img_path,
+        }
+    )
