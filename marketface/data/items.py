c20bd34862289364ab8f80a47ceb8a6cbfbfeca9
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2025-07-20 07:41:41 -0300

    new class BaseRepo

--- marketface/data/items.py
+++ marketface/data/items.py
@@ -1,6 +1,7 @@
 from typing import Optional, Iterator, Dict
 
 from pocketbase import PocketBase
+from pocketbase.services.record_service import RecordService
 from pydantic import BaseModel, Field
 
 
@@ -27,11 +28,11 @@ class Item(PocketBaseModel):
         from_attributes = True
 
 
-class ItemRepo:
+class BaseRepo:
 
-    def __init__(self, client: PocketBase):
-        self.client = client
-        self.collection = self.client.collection("items")
+    def setup(self, client: PocketBase, collection_name: str) -> None:
+        self.client: PocketBase = client
+        self.collection: RecordService = self.client.collection(collection_name)
 
     def _validate(self, data) -> Optional[Item]:
         if not data:
@@ -41,9 +42,6 @@ class ItemRepo:
     def first(self, query: str) -> Optional[Item]:
         record = self.collection.get_first_list_item(query)
         return self._validate(record)
-
-    def by_url(self, url: str) -> Optional[Item]:
-        return self.first(f'url = "{url}"')
 
     def list(self, start: int, count: int, params: Optional[Dict] = None) -> Iterator[Optional[Item]]:
         params = params or {}
@@ -61,19 +59,28 @@ class ItemRepo:
                 yield self._validate(item)
             page += 1
 
-    def incomplete(self) -> Iterator[Optional[Item]]:
-        yield from self.all({"filter": "title = ''"})
-
     def update(self, item: Item) -> Optional[Item]:
         record = self.collection.update(item.id, item.model_dump())
         return self._validate(record)
-
-    def mark_deleted(self, item: Item) -> Optional[Item]:
-        item.deleted = True
-        return self.update(item)
 
     def create(self, data: Dict) -> Optional[Item]:
         item = self._validate(data)
         if not item:
             return None
         self.collection.create(item.model_dump())
+
+
+class ItemRepo(BaseRepo):
+
+    def __init__(self, client: PocketBase):
+        self.setup(client, "items")
+
+    def by_url(self, url: str) -> Optional[Item]:
+        return self.first(f'url = "{url}"')
+
+    def incomplete(self) -> Iterator[Optional[Item]]:
+        yield from self.all({"filter": "title = ''"})
+
+    def mark_deleted(self, item: Item) -> Optional[Item]:
+        item.deleted = True
+        return self.update(item)
