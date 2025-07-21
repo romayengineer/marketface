26f93c8fb202ad5e6403fe91719ff39cfedb49c0
Maximiliano Romay Figueroa <mromay@hyphencare.com>
2025-07-21 18:48:50 -0300

    cleanse html

--- marketface/cli/get_leafs.py
+++ marketface/cli/get_leafs.py
@@ -3,21 +3,26 @@ from marketface.data.backend import auth
 from marketface.data.items import ItemRepo
 from marketface.logger import getLogger
 from marketface.page.cleanse import get_leaf_tags
+from random import randint
 
 
 logger = getLogger("get_leafs")
 
+logger.info = print
 
 def main() -> None:
     client = auth()
 
     items_repo = ItemRepo(client)
-    for item in items_repo.all({"filter": "html != ''"}):
-        logger.info("URL: %s", item.url)
-        html = str(item.html)
-        for leaf in get_leaf_tags(html):
-            logger.info(leaf)
-        break
+    while True:
+        for item in items_repo.all({"filter": "html != ''"}):
+            if randint(0, 100) != 50:
+                continue
+            logger.info("URL: %s", item.url)
+            html = str(item.html)
+            for leaf in get_leaf_tags(html):
+                logger.info(leaf)
+            return
 
 
 if __name__ == "__main__":
--- marketface/page/cleanse.py
+++ marketface/page/cleanse.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from bs4.element import Tag
 
-from typing import Set, List, Iterator, cast
+from typing import Set, List, Iterator, Optional, cast
 
 
 def clean_html_attributes(html_content: str, unwanted_attrs: Set[str]) -> str:
@@ -15,7 +15,27 @@ def clean_html_attributes(html_content: str, unwanted_attrs: Set[str]) -> str:
 def is_leaf(tag: Tag) -> bool:
     return tag.find(True, recursive=False) is None
 
+def has_text(tag: Tag) -> bool:
+    for _ in tag._all_strings(strip=True):
+        return True
+    return False
+
+def get_parent(tag: Tag, up: int = 1) -> Tag:
+    if up > 0 and tag.parent:
+        return get_parent(tag.parent, up = up - 1)
+    return tag
+
 def get_leaf_tags(html_content: str) -> Iterator[Tag]:
     soup = BeautifulSoup(html_content, 'html.parser')
-    for leaf in soup.find_all(is_leaf):
-        yield cast(Tag, leaf)
+    # for leaf in soup.find_all(is_leaf):
+    for leaf in soup.find_all(lambda tag: is_leaf(tag) and has_text(tag)):
+        parent = get_parent(cast(Tag, leaf), up=2)
+        text = parent.get_text(separator=" | ", strip=True)
+        for skip_message in [
+            "Envía un mensaje al vendedor",
+            "Hola. ¿Sigue disponible?",
+            "Más información",
+        ]:
+            if skip_message in text:
+                break
+        yield parent
