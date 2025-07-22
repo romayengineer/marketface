26da9c4bbf1a8bc58ae138cd2d198e267b0bdeb2
Maximiliano Romay Figueroa <mromay@hyphencare.com>
2025-07-22 04:24:18 -0300

    new functions in cleanse utils

--- marketface/cli/get_leafs.py
+++ marketface/cli/get_leafs.py
@@ -2,8 +2,9 @@
 from marketface.data.backend import auth
 from marketface.data.items import ItemRepo
 from marketface.logger import getLogger
-from marketface.page.cleanse import get_leaf_tags
+from marketface.page import cleanse
 from random import randint
+from bs4 import BeautifulSoup
 
 
 logger = getLogger("get_leafs")
@@ -14,15 +15,42 @@ def main() -> None:
     client = auth()
 
     items_repo = ItemRepo(client)
+
+    count = 1
+    max_count = 10
     while True:
         for item in items_repo.all({"filter": "html != ''"}):
-            if randint(0, 100) != 50:
+            if randint(0, 100) != 0:
                 continue
-            logger.info("URL: %s", item.url)
+            print("URL: ", item.url)
             html = str(item.html)
-            for leaf in get_leaf_tags(html):
-                logger.info(leaf)
-            return
+            # for leaf in cleanse.get_parent_tags(html):
+            #     logger.info(leaf.get_text(separator=" | ", strip=True))
+            soup = BeautifulSoup(html, 'html.parser')
+            # logger.info(soup.get_text(separator=" |\n", strip=True))
+            text_to_skip = [
+                "Compartir",
+                "Guardar",
+                "Enviar",
+                "Enviar mensaje",
+                "Envía un mensaje al vendedor",
+                "Hola. ¿Sigue disponible?",
+                "Hola. ¿Sigue estando disponible?",
+                "Detalles del vendedor",
+            ]
+            lines = []
+            for text in soup._all_strings(strip=True):
+                if text == "Publicidad":
+                    break
+                if text in text_to_skip:
+                    continue
+                lines.append(text)
+            print(" |\n".join(lines))
+            print("-" * 80)
+            print("-" * 80)
+            count += 1
+            if count >= max_count:
+                return
 
 
 if __name__ == "__main__":
--- marketface/page/cleanse.py
+++ marketface/page/cleanse.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from bs4.element import Tag
+from bs4._typing import _OneElement
 
 from typing import Set, List, Iterator, Optional, cast
 
@@ -20,22 +21,28 @@ def has_text(tag: Tag) -> bool:
         return True
     return False
 
-def get_parent(tag: Tag, up: int = 1) -> Tag:
+def get_parent(tag: _OneElement, up: int = 1) -> _OneElement:
     if up > 0 and tag.parent:
         return get_parent(tag.parent, up = up - 1)
     return tag
 
-def get_leaf_tags(html_content: str) -> Iterator[Tag]:
+def get_leaf_tags(html_content: str) -> Iterator[_OneElement]:
     soup = BeautifulSoup(html_content, 'html.parser')
-    # for leaf in soup.find_all(is_leaf):
     for leaf in soup.find_all(lambda tag: is_leaf(tag) and has_text(tag)):
-        parent = get_parent(cast(Tag, leaf), up=2)
+        yield leaf
+
+def get_parent_tags(html_content: str, up: int = 2) -> Iterator[_OneElement]:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    for leaf in soup.find_all(lambda tag: is_leaf(tag) and has_text(tag)):
+        parent = get_parent(leaf, up=up)
         text = parent.get_text(separator=" | ", strip=True)
-        for skip_message in [
-            "Envía un mensaje al vendedor",
-            "Hola. ¿Sigue disponible?",
-            "Más información",
-        ]:
-            if skip_message in text:
-                break
+        if any(map(
+            lambda m: m in text,
+            [
+                "Envía un mensaje al vendedor",
+                "Hola. ¿Sigue disponible?",
+                "Hola. ¿Sigue estando disponible?",
+                "Más información",
+            ])):
+                continue
         yield parent
